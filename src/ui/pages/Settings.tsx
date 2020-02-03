32fbbca61e350271893fb3369f7c788427ca9529
Tim Speelman <timspeelman@live.nl>
2020-02-03 15:55:15 +0100

    Add my id to settings

--- src/ui/pages/Settings.tsx
+++ src/ui/pages/Settings.tsx
@@ -1,16 +1,27 @@
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, Divider } from "@material-ui/core";
 import { default as React } from "react";
 import { ClearCache, ToggleConsole } from "../../commands/Command";
 import { useCommand } from "../hooks/useCommand";
+import { useLocalState } from "../hooks/useLocalState";
 
 export function Settings() {
     const { dispatch } = useCommand();
+    const { state } = useLocalState();
     return (
         <Box pt={3}>
-            <p>In de browsercache worden de appgegevens opgeslagen.</p>
-            <Button variant="outlined" onClick={() => dispatch(ClearCache({}))} >Cache Legen</Button>
-            <p>In de console kunnen loggegevens worden getoond.</p>
-            <Button variant="outlined" onClick={() => dispatch(ToggleConsole({}))} >Loggen aan-/uitzetten</Button>
+            <Box mb={3}>
+                <p>Uw ID: {state.myId}</p>
+            </Box>
+            <Box mb={3}>
+                <Divider />
+                <p>In de console kunnen loggegevens worden getoond.</p>
+                <Button variant="outlined" onClick={() => dispatch(ToggleConsole({}))} >Loggen aan-/uitzetten</Button>
+            </Box>
+            <Box >
+                <Divider />
+                <p>In de browsercache worden de appgegevens opgeslagen.</p>
+                <Button variant="outlined" onClick={() => dispatch(ClearCache({}))} >Cache Legen</Button>
+            </Box>
         </Box>
     );
 }
